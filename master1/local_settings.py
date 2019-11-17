0516fae7d21f1b7daffeb0ed15c83389f8e67aa4
sandhya <sandhyamishra@unomaha.edu>
2019-11-17 00:29:21 -0600

    first

---
+++ master1/local_settings.py
@@ -0,0 +1,15 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
